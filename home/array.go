5c6faae6b8cd11063dda5e19fe71d15994c4a101
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2026-03-02 18:59:22 +0600

    add array

--- home/array.go
+++ home/array.go
@@ -1,5 +1,16 @@
 package main
 
+import "fmt"
+
 func array(){
-	// var array_name = [length]datatype{values}
+	// var array_name = [length]datatype{values} # here length is defined
+	// var array_name = [...]datatype{values} # here length is inferred
+
+	var arr1 = [3]int{1,2,3} // set variable
+	arr2 := [...]int{4,5,6,7,8}
+	arr3 := [...]string{"Go", "array", "Are", "Powerful"}
+
+	fmt.Println(arr1,arr2,arr3)
+	
+
 }
