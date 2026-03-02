31df6738d4f93ae2262c247928ea4232c5ab0435
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2026-03-02 19:36:14 +0600

    now i know go basic syntex

--- home/Maps.go
+++ home/Maps.go
@@ -8,6 +8,6 @@ func _Maps() {
   var a = map[string]string{"brand": "Ford", "model": "Mustang", "year": "1964"}
   b := map[string]int{"Oslo": 1, "Bergen": 2, "Trondheim": 3, "Stavanger": 4}
 
-  fmt.Printf("a\t%v\n", a)
-  fmt.Printf("b\t%v\n", b)
+  fmt.Println(a)
+  fmt.Println(b)
 }
