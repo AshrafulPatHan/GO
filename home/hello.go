2f26866c247a84471311395cc98900b8c07f2926
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2026-03-02 19:10:50 +0600

    learn loop

--- home/hello.go
+++ home/hello.go
@@ -14,4 +14,5 @@ func main() {
     math()
     if_Else()
     _switch()
+    Loop()
 }
---
+++ home/loop.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func Loop() {
+	for  i:=0; i < 5; i++ {
+		fmt.Println(i)
+	}
+}
